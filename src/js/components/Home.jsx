d6650a04bb026f1c6ef8bb441d71ab9883a10a6b
marioramirezlopez98-del <marioramirez.lopez98@gmail.com>
2026-05-22 21:37:48 +0000

    Desarrollo del proyecto Semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [lightColor, setLightColor] = useState("")
+	
+
+	return (
+		<div className="text-center d-flex flex-column align-items-center">
+			<div className="d-flex flex-column bg-dark p-1">
+			</div>
+            <div className="d-inline-flex bg-dark m-0">
+				<div className="d-flex flex-column align-items-center gap-2 p-2">
+					<div className={ `col-4 rounded-4 p-3  ${lightColor === "red" ? "redlight shadowred" : "gray"}`} onClick={()=>setLightColor(lightColor == "red" ? "gray" : "red")}></div>
+					<div className={`col-4 rounded-4 p-3 ${lightColor === "yellow" ? "yellowlight shadowyellow" : "gray"}`} onClick={()=>setLightColor(lightColor === "yellow" ? "gray" : "yellow")}></div>
+					<div className={`col-4 rounded-4 p-3 ${lightColor === "green" ? "greenlight shadowgreen" : "gray"}`} onClick={()=>setLightColor(lightColor == "green" ? "gray" : "green")}></div>
+				</div>
+			</div>
 		</div>
 	);
 };
+
+
 
 export default Home;
